9ff4907d0bc1784b1d696a750db5986ccf990bac
Antonio Pitasi <antonio@pitasi.dev>
2024-12-19 16:58:47 +0100

    gosmic: fix Go anto.pt/x redirect

--- gosmic/x.go
+++ gosmic/x.go
@@ -3,7 +3,13 @@ package main
 import "net/http"
 
 func x(mux *http.ServeMux) {
-	mux.HandleFunc("/x", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /x", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusMovedPermanently)
+		w.Write([]byte(`<head><meta name="go-import" content="anto.pt/x git https://github.com/Pitasi/x">
+<meta http-equiv="refresh" content="0;URL='https://github.com/Pitasi/x'">
+<body>Redirecting you to the <a href="https://github.com/Pitasi/x">project page</a>...`))
+	})
+	mux.HandleFunc("GET /x/{path...}", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMovedPermanently)
 		w.Write([]byte(`<head><meta name="go-import" content="anto.pt/x git https://github.com/Pitasi/x">
 <meta http-equiv="refresh" content="0;URL='https://github.com/Pitasi/x'">
